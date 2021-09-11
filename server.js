8474ffe7b3148a30abdc410ad364a5b4da7f3669
Craig McGowan <craigmcgowan15@gmail.com>
2021-09-11 14:43:58 -0400

    Tip calculator express application created

--- server.js
+++ server.js
@@ -15,3 +15,8 @@ app.get("/greeting", (req, res) => {
 app.get('/greeting/:name', (req, res) => {
     res.send(`Yo ${req.params.name}, so glad you made it!`)
 })
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = req.params.total, tipPercentage = req.params.tipPercentage
+    res.send(`Your bill total was ${total}, so a ${tipPercentage}% tip would be $${total*tipPercentage/100}`)
+})
